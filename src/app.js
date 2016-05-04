e9293040f6492b6fc19f310f576aeaecc0cb75c0
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-05-04 13:29:03 +0200

    Reuse the dnode thingies

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
+import dheaders from 'dheaders';
+import dhttpAuth from 'dhttp-auth';
 import app from 'dkoa';
 
 import Router from './router.js';
 
+app.use(dheaders);
+app.use(dhttpAuth);
 const router = Router();
 app
   .use(router.routes())
